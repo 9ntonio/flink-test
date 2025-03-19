8e65ff94a165900a318400df1e6bf588bee919af
Antonio <antonio@spacebase.one>
2025-03-19 15:21:11 -0700

    Add AnimatedBox to components exports

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,8 @@
-export { default as Button } from './Button';\nexport type { ButtonProps } from './Button';\n\nexport { default as Card } from './Card';\nexport type { CardProps } from './Card';
+export { default as Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { default as Card } from './Card';
+export type { CardProps } from './Card';
+
+export { default as AnimatedBox } from './AnimatedBox/AnimatedBox';
+export type { AnimatedBoxProps } from './AnimatedBox/AnimatedBox';
