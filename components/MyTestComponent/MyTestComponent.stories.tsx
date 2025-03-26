2d0ccb7793c0acdb6ad4d88b19c5b1e6e4b8dcf4
Antonio <Antonio.Almena@missingcorner.com>
2025-03-26 11:29:57 -0700

    [9ntonio][generate-react-cli]

---
+++ components/MyTestComponent/MyTestComponent.stories.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import MyTestComponent from './MyTestComponent';
+
+export default {
+  title: "MyTestComponent",
+};
+
+export const Default = () => <MyTestComponent />;
+
+Default.story = {
+  name: 'default',
+};
---
+++ components/MyTestComponent/MyTestComponent.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface MyTestComponentProps {}
+
+const MyTestComponent: FC<MyTestComponentProps> = () => (
+  <div>
+    MyTestComponent Component
+  </div>
+);
+
+export default MyTestComponent;
