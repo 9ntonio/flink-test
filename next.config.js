4b013099ce8d4e4fbe6664e2b8176232865aa404
Antonio <Antonio.Almena@missingcorner.com>
2025-03-26 12:20:02 -0700

    [9ntonio][next Config]

--- next.config.js
+++ next.config.js
@@ -3,9 +3,12 @@ const million = require('million/compiler');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Essential for static site generation
   images: {
+    unoptimized: true, // Required for static export
     domains: ['images.ctfassets.net'],
   },
+  trailingSlash: true, // Creates proper /about/index.html files
 };
 
 module.exports = million.next(nextConfig, { auto: true });
