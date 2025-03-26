b83006e24540720c12e588ba68fa8289c638caa4
Antonio <Antonio.Almena@missingcorner.com>
2025-03-26 11:33:07 -0700

    [9ntonio][generate-react-cli]

---
+++ components/Test2Comp/Test2Comp.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface Test2CompProps {}
+
+const Test2Comp: FC<Test2CompProps> = () => (
+  <div>
+    Test2Comp Component
+  </div>
+);
+
+export default Test2Comp;
